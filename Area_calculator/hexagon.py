8a7f73c9a35dac23e7c00a766ae37d3c16a073c1
srullsim <simchonisrael@gmail.com>
2025-06-25 11:08:23 +0300

    add hexagon class.py✓

---
+++ Area_calculator/hexagon.py
@@ -0,0 +1,18 @@
+from  base import Shape
+import math
+
+
+class Hexagon(Shape):
+
+    def __init__(self, side: float|int):
+        self.side = side
+
+
+    def getArea(self):
+        return (3 * math.sqrt(3) * self.side ** 2) / 2
+
+    def get_perimeter(self):
+        return self.side * 6
+
+    def __str__(self):
+        return f"each side in the hexagon is {self.side} "
